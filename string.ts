d985aeabbd2d77bc351e84638c2b0663a51fa690
ElPappo <pappolayla@hotmail.com>
2024-10-26 16:46:22 -0300

    cambiosLogita

--- string.ts
+++ string.ts
@@ -1,5 +1,7 @@
+import * as rs from "readline-sync"
+
 const paragraph: string =
-   `Urania. Qué nombre. A la gente le gusta, le parece sonoro, original, raro. A ella también,
+  `Urania. Qué nombre. A la gente le gusta, le parece sonoro, original, raro. A ella también,
     aunque le evoca sólo cosas tristes. Todo le evoca cosas tristes, ahora que ha vuelto.
     Pero ¿cuándo no, desde que se fue de esta ciudad, a la que creía no volvería nunca? 
     ¿Y cómo era posible que alguien se llamara así en una familia de dominicanos católicos, 
@@ -11,37 +13,35 @@ const paragraph: string =
 
 //devuelve true si searchTerm se encuentra en fullText
 function isTextInPara(fullText: string, searchTerm: string): boolean {
-  
+
   return fullText.toLowerCase().includes(searchTerm.toLowerCase());
 }
 
 //completar la funcion para que devuelva la cantidad de veces que searchTerm aparece dentro de fullText
 function howManyTextInPara(fullText: string, searchTerm: string): number {
-  
+
   // Textos a minúsculas
-   const todoMinuscula = fullText.toLowerCase();
-   const MinusculaSearch = searchTerm.toLowerCase();
-   //contador
-   let count = 0;
-   // indice de inicio
-   let startIndex = 0;
+  const todoMinuscula = fullText.toLowerCase();
+  const MinusculaSearch = searchTerm.toLowerCase();
+  //contador
+  let count: number = 0;
+  // indice de inicio
+  let startIndex: number = 0;
 
-   // Bucle para buscar 
-   for (startIndex = todoMinuscula.indexOf(MinusculaSearch); 
-   startIndex !== -1; startIndex = todoMinuscula.indexOf(MinusculaSearch, startIndex += todoMinuscula.length)) {
-   count ++;
-   
+  // Bucle para buscar 
+  startIndex = todoMinuscula.indexOf(MinusculaSearch);
+  while (startIndex !== -1) {
+    count++;
+    startIndex = todoMinuscula.indexOf(MinusculaSearch, startIndex + 1);
+  }
+  return count;
 }
 
-return count;
-
-      
-}
-const searchTerm = "nombre";
+const searchTerm = rs.question(`Ingrese el Texto: `);
 const result = howManyTextInPara(paragraph, searchTerm);
 console.log(`La palabra '${searchTerm}' aparece ${result} veces en el texto.`);
 
-
+//El motodo indexof, devuelve la primera posicion donde aparece el termino buscado, sino la encuentra devuelve -1.
 
 
 
